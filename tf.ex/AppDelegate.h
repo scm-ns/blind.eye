1d1d181df3d7e33c9ebe7caa9d347a68ae88e368
scm197 <scm197@scm197s-mac.local>
2016-11-18 19:58:39 -0500

    Initial Commit

---
+++ tf.ex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tf.ex
+//
+//  Created by scm197 on 11/18/16.
+//  Copyright © 2016 scm197. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tf.ex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tf.ex
+//
+//  Created by scm197 on 11/18/16.
+//  Copyright © 2016 scm197. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
