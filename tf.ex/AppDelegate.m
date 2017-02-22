0725f0ccb433d3b88efae1a4b31e3d6f942cd236
scm-ns <scm197@rutgers.edu>
2017-02-22 06:38:08 -0500

    minor changes

--- tf.ex/AppDelegate.m
+++ tf.ex/AppDelegate.m
@@ -52,4 +52,10 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)respondsToSelector:(SEL)aSelector
+{
+    NSLog(NSStringFromSelector(aSelector));
+    return [super respondsToSelector:aSelector];
+}
+
 @end
